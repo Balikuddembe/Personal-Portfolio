b52fbf0eb9980a4f8c2d72694844f26e0f5bfd06
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-09-08 21:06:52 +0300

    Implemented a Form Validation

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ function toggleClasses() {
 toggleClasses();
 toggleClasses();
 
-//form validation
+// form validation
+
 const form = document.getElementById('form');
 const error = document.querySelector('.error');
-const name = document.getElementById('name');
 const email = document.getElementById('email');
-const message = document.getElementById('message');
-
-form.addEventListener('submit',function(event){
-  let messageError = [];
-  if(email.value!=email.value.toLowerCase()){
+form.addEventListener('submit', (event) => {
+  const messageError = [];
+  if (email.value !== email.value.toLowerCase()) {
     event.preventDefault();
-    messageError.push("E-mail should be in lowercase Letters Only !!!");
+    messageError.push('E-mail should be in lowercase Letters Only !!!');
   }
-  if(messageError.length > 0){
+  if (messageError.length > 0) {
     error.innerHTML = messageError.join();
   }
-})
-
-
-
+});
